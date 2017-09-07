17fe2178a4244c1c3a9bd3ebc9fbd95fec764530
dessurya <fourline66@gmail.com>
2017-09-07 10:31:02 +0700

    navigation and home

--- resources/views/frontend/home-page/index.blade.php
+++ resources/views/frontend/home-page/index.blade.php
@@ -16,11 +16,17 @@
 	#navigasi #copy-right{
 		background-image: url("{{ asset('amadeo/images-base/dekstop-dot-tekstur.png') }}");
 	}
+	#main-content #one.main-slide{
+		background-image: url("{{ asset('amadeo/images-base/home-bg-one.jpg') }}");
+	}
+	#main-content.show-discrip #one.main-slide{
+		background-image: url("{{ asset('amadeo/images-base/home-bg-two.jpg') }}");
+	}
 </style>
 @endsection
 
 @section('content')
-	<div id="navigasi">
+	<div id="navigasi" class="animation-element">
 		<a href="{{ route('frontend.dekstop') }}">
 			<img id="logo-tingkat" src="{{ asset('amadeo/images-base/logo-tingkat.png') }}">
 		</a>
@@ -31,11 +37,11 @@
 					home
 				</a>
 			</div>
-			<div class="list">
+			<div class="list slide-down-actived">
 				<a>
 					about us
 				</a>
-				<div class="slide-down">
+				<div class="slide-down-content">
 					<a href="">
 						our commitment
 					</a>
@@ -67,20 +73,30 @@
 		</div>
 	</div>
 	<div id="main-content" class="animation-element">
-		<div id="setup-space">
-			<div id="vertical-midle">
-				<div id="welcome" class="col">
-					<h2>welcome to</h2>
-				</div>
-				<div id="tingkat" class="col">
-					<a href="{{ route('frontend.home') }}">
-						<img src="{{ asset('amadeo/images-base/logo-tingkat.png') }}">
-					</a>
-				</div>
+		<div id="one" class="main-slide">
+			<h1>we love to design</h1>
+			<h1>beyond your dreams</h1>
+		</div>
+		<div id="two" class="main-slide">
+			<div id="content">
+				<h1>we are <big>tingkat</big></h1>
+				<p>
+					In the year of 2016, we have created our Commercial Interior and Architecture Division that is called TINGKAT. At Tingkat we believe to be effective, design for workspaces needs to be a reflection of your company’s values, your people and your organizational processes. By taking the time to understand your company and your processes, a practical, unique and extraordinary design solution can be achieved.
+				</p>
+				<p>
+					Now we are positioning ourselves as designworks, with the vision to be well known most design-centric company in interior architecture industry.
+					Visit our facility and our super team will guide you to experience the transformation of your dreams into reality and even more!
+				</p>
 			</div>
 		</div>
+
 	</div>
 @endsection
 
 @section('script')
+<script type="text/javascript">
+	window.setTimeout(function() {
+	    $("#main-content").addClass('show-discrip');
+  }, 3832);
+</script>
 @endsection
